ea74fc20a58d5d974d57eb9040032be7d736a853
LucasSaccoParisi <lucas.sparisi@senacsp.edu.br>
2025-05-14 10:23:58 -0300

    Create ex01.c

---
+++ ex01.c
@@ -0,0 +1,23 @@
+// 1 - Crie uma matriz 3x3 de números inteiros, associe o valor 9 ao elemento central desta matriz. Exiba a matriz na tela de forma adequada.
+
+#include <stdio.h>
+
+int main(){
+    int m[3][3];
+    
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            m[i][j] = 9;
+            printf("%d ", m[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
+
+// Resultado:
+
+// 9 9 9 
+// 9 9 9 
+// 9 9 9 
